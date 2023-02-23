79d45461b8601838d36032d2e236f9fb912a4e28
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-23 21:06:10 +0600

    Public theme setup completed

---
+++ laraPortfolio/resources/views/layouts/inc/public-main/home.blade.php
@@ -0,0 +1,25 @@
+<!-- Home Section Start -->
+<section class="home active section" id="home">
+    <div class="container">
+        <div class="row">
+            <div class="home-info padd-15">
+                <h3 class="hello">
+                    Hello, my name is <span class="name">Alini Mousaad</span>
+                </h3>
+                <h3 class="my-profession">
+                    I'm a <span class="typing">Web Designer</span>
+                </h3>
+                <p>
+                    I'm a web designer with extensive experience for over 10
+                    years. My expertise is to create and design website, graphic
+                    design, and many more...
+                </p>
+                <a href="#" class="btn">Download CV</a>
+            </div>
+            <div class="home-img padd-15">
+                <img src="{{ asset('public-assets/images/hero.jpg') }}" alt="hero" />
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Home Section End -->
