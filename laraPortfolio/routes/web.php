b584b3bb9798925342af11d1c90187dbd4db6086
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2023-02-23 22:13:14 +0600

    Admin theme setup completed

--- laraPortfolio/routes/web.php
+++ laraPortfolio/routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
     return view('public.index');
 });
 
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
